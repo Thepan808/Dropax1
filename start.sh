fd1894253da910d590efa5a0752a6aabf1678757
The Vampiro Elegant <streetdemonic003@gmail.com>
2023-02-13 00:18:43 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone https://github.com/Thepan808/pinterest-telegram-bot ok && cd pinterest-telegram-bot && pip3 install -U -r requirements.txt
-python3 pinterest_telegram_bot.py
+git clone https://github.com/Thepan808/Vamplegendas ok && cd Vamplegendas && pip3 install -U -r requirements.txt
+python3 channelbot.py
